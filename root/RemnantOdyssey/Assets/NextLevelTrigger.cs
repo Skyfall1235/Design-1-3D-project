2d8831a897180dbd35cb2bdf658b149d8882f396
BlackM3rcy <joeycalderone23@gmail.com>
2023-05-04 17:35:33 -0400

    fuck this shit

---
+++ root/RemnantOdyssey/Assets/NextLevelTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.CompareTag("Player"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                SceneManager.LoadScene(2);
+            }
+            if(SceneManager.GetActiveScene().buildIndex == 2)
+            {
+                SceneManager.LoadScene(3);
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 3)
+            {
+                SceneManager.LoadScene(4);
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 4)
+            {
+                SceneManager.LoadScene(5);
+            }
+        }
+
+    }
+}
