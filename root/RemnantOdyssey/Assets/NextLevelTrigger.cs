95a84f36a6699c5d2da998cb12155fa316012fa9
Flynn Niccolo <headphonesgeek@gmail.com>
2023-05-04 16:28:55 -0400

    oh my god no time

---
+++ root/RemnantOdyssey/Assets/NextLevelTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTrigger : MonoBehaviour
+{ 
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.CompareTag("Player"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                SceneManager.LoadScene(2);
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 3)
+            {
+                SceneManager.LoadScene(2);
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 2)
+            {
+                if (toPolice)
+                {
+                    SceneManager.LoadScene(3);
+                } else {
+                    SceneManager.LoadScene(4);
+                }
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 4)
+            {
+                SceneManager.LoadScene(5);
+            }
+        }
+    }
+}
